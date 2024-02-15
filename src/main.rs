03bb6afbc117dc52559485c65b6dfa8b8fd20cd0
Narcissus <dzcdada123@gmail.com>
2024-02-15 18:42:23 +0800

    finish

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use std::{env, fs};
+use std::{env, process};
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
-
-    let content = fs::read_to_string(file_path).expect("Should have been able to read the file");
-
-    println!("With text: \n{content}");
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
 }
